9917d330c776adc2fc9e8bfa164933aa26d0df9f
Matthew Hurley <142431276+Murfew@users.noreply.github.com>
2024-01-10 10:44:03 -0500

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !="number" || typeof num2 != "number") {return "ERROR"}
+
+    let upperBound = Math.max(num1, num2)
+    let lowerBound = Math.min(num1, num2)
+
+    let topSum = (upperBound * (upperBound + 1)) / 2
+    let bottomSum = ((lowerBound * (lowerBound + 1)) / 2) - 1
+
+    return topSum - bottomSum
+
 
 };
 
