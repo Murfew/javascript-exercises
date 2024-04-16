730c1ce179f62670efc76d8c409c423e90f250f3
Matthew Hurley <matthew.hurley953@gmail.com>
2024-04-16 16:13:21 -0400

    full solution to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if (value < 0) {
+        return "OOPS"
+    }
 
+    if (value == 0) {
+        return 0
+    }
+    
+    fibs = [1, 1]
+
+    for (let i = 2; i < value; i++) {
+        fibs[fibs.length] = fibs[fibs.length - 1] + fibs[fibs.length - 2]
+    }
+
+    return fibs[fibs.length - 1]
 };
 
 // Do not edit below this line
