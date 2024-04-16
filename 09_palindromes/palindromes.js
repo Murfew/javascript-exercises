9dabb1f7687c067ab7b8dd460f3c282228ad6007
Matthew Hurley <matthew.hurley953@gmail.com>
2024-04-16 15:51:36 -0400

    final solution. added some input cleanup to fix certain cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
 const palindromes = function (word) {
-    let chars = word.split("")
+    let chars = []
+
+    for (let i = 0; i < word.length; i++) {
+        if (isAlphanumeric(word[i])) {
+            chars.push(word[i].toLowerCase())
+        }
+    }
+
     let lenEachHalf = Math.trunc(chars.length / 2)
     if (chars.length % 2 != 0) {
         chars.splice(lenEachHalf, 1)
@@ -16,6 +23,10 @@ const palindromes = function (word) {
 
     return true
 };
+
+function isAlphanumeric(str) {
+    return /^[a-zA-Z0-9]+$/.test(str)
+}
 
 // Do not edit below this line
 module.exports = palindromes;
