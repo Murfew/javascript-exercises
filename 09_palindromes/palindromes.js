653c741a177b7146b9572a22f99a8c98c909ebd9
Murfew <142431276+Murfew@users.noreply.github.com>
2024-04-16 14:47:23 -0400

    first attempt at solving the problem. using 2 arrays and checking them with a loop

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let chars = word.split("")
+    let lenEachHalf = Math.trunc(chars.length / 2)
+    if (chars.length % 2 != 0) {
+        chars.splice(lenEachHalf, 1)
+    }
 
+    let leftHalf = chars.slice(0, lenEachHalf)
+    let rightHalf = chars.slice(lenEachHalf, chars.length).reverse()
+
+    for (let i = 0; i < lenEachHalf; i++) {
+        if (leftHalf[i] != rightHalf[i]) {
+            return false
+        }
+    }
+
+    return true
 };
 
 // Do not edit below this line
