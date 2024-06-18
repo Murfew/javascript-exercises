2bc112571c740e0789e310afe8f1bcef601f31cd
Matthew Hurley <142431276+Murfew@users.noreply.github.com>
2024-06-18 17:36:42 -0400

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const ages = array.map((person) => {
+        if (person.yearOfDeath == undefined) {
+            let currentDate = new Date()
+            return {name: person.name, age: currentDate.getFullYear() - person.yearOfBirth}
+        } else {
+            return {name: person.name, age: person.yearOfDeath - person.yearOfBirth}
+        }
+    })
+
+    return ages.reduce((oldest, current) => {
+        if (oldest.age < current.age) {
+            return current
+        } else {
+            return oldest
+        }
+    })
 };
 
 // Do not edit below this line
